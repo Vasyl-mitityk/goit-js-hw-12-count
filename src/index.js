38b36ee4574f49c87d027a918a0396af8c27e072
Vasyl-mitityk <Vipsvob@gmail.com>
2021-07-15 01:45:11 +0300

    поправки

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// import './styles.css';
+import refs from './js/refs';
+import fetchCountries from './js/fetchCountries';
+import addMarkup from './js/add-markup';
+import debounce from 'lodash.debounce';
+
+refs.countryInput.addEventListener('input', debounce(countryInputHandler, 500));
+
+function countryInputHandler(event) {
+  fetchCountries(event.target.value)
+    .then(countries => {
+      refs.markup.innerHTML = '';
+      addMarkup(countries);
+    })
+    .catch(error => {
+      return;
+    });
+}
